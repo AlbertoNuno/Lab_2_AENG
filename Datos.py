4e268e55b4bf1c42357dcd04eb2b1a085aaaa315
AlbertoNuno <anunoguevara@gmail.com>
2020-02-27 08:41:19 -0600

    Funciones nuevas

---
+++ Datos.py
@@ -0,0 +1,9 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- Proyecto: Laboratorio 2, Microestructura y Sistema de trading                           -- #
+# -- Codigo: datos.py - script con datos de uso en proyecto                               -- #
+# -- Rep:https://github.com/AlbertoNuno/Lab_2_AENG     -- #
+# -- Autor: Alberto Nuño                                                              -- #
+# -- ------------------------------------------------------------------------------------ -- #
+
+
+token = '40a4858c00646a218d055374c2950239-f520f4a80719d9749cc020ddb5188887'
